5418f80a127ef507c04e57822cf165fdba2938d5
Anton Ivanov <anton@ivanov.hk>
2023-03-26 20:32:56 -0700

    feat: implement get summoner call

--- backend/lol-stats/app.ts
+++ backend/lol-stats/app.ts
@@ -1,12 +1,13 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { getSummonerByName } from './api/getSummonerByName';
+import { Regions } from 'twisted/dist/constants';
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+        const summoner = await getSummonerByName('Doublelift', Regions.AMERICA_NORTH);
         return {
             statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello world',
-            }),
+            body: JSON.stringify(summoner),
         };
     } catch (err) {
         console.log(err);
