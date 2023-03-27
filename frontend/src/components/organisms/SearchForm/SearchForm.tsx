8e88519810866c16aafe0c09b997a3bd2038eb9d
Anton Ivanov <anton@ivanov.hk>
2023-03-26 23:04:13 -0700

    feat: fetch actual data from server

--- frontend/src/components/organisms/SearchForm/SearchForm.tsx
+++ frontend/src/components/organisms/SearchForm/SearchForm.tsx
@@ -1,12 +1,31 @@
+import { getMatchesByName } from "../../../api/api";
+import { useStatsContext } from "../../../state/StatsContext";
+import { LolRegion } from "../../../types/LolRegion";
 import { Button } from "../../atoms/Button/Button";
 import { SummonerNameInput } from "../../atoms/SummonerNameInput/SummonerNameInput"
 import './SearchForm.scss';
 
+const HARDCODED_REGION = LolRegion.AMERICA_NORTH;
+
 export const SearchForm = () => {
+    const { isLoading, setIsLoading, summonerName, setStats } = useStatsContext();
+    const fetchStats = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await getMatchesByName(summonerName, HARDCODED_REGION);
+            setStats(response);
+        } catch (e) {
+            console.error(e);
+        }
+
+        setIsLoading(false);
+    }
+
     return (
         <div className="SearchForm">
             <SummonerNameInput />
-            <Button disabled={false} onClick={() => {}}>
+            <Button disabled={isLoading} onClick={fetchStats}>
                 Get Stats
             </Button>
         </div>
